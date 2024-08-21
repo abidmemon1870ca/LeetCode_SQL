db1e5ee76b5bdce1de54311403cf3ab68af06512
Abid Memon <abidmemon1870ca@gmail.com>
2024-08-21 09:53:06 -0400

    Time: 675 ms (29.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,7 @@
+select distinct
+round((sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)/count(order_date))*100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in
+(   select customer_id, min(order_date)
+    from Delivery
+    group by customer_id);
